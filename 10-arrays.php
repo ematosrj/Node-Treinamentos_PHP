4ad9ac95af201f63de7c594b6ecb74eac750014f
ENES DA SILVA MATOS <e.matos@pm.ba.gov.br>
2023-12-14 16:04:28 -0300

    ok

--- 10-arrays.php
+++ 10-arrays.php
@@ -1,1 +1,26 @@
-<?php ?>
+<?php
+
+$motos = array();
+$motos[1] = "Yamaha";
+$motos[2] = "Suzuki";
+$motos[3] = "Honda";
+$motos[4] = "Suzuki";
+$motos[5] = "YBR";
+$motos[6] = "Harley Davidson";
+
+echo '<br>';
+echo $motos[1];
+echo '<br>';
+echo $motos[3];
+echo '<br>';
+echo $motos[5];
+
+$totalClientes = count($motos);
+echo '<br>';
+echo '<br>';
+
+echo $totalClientes;
+
+
+
+?>
