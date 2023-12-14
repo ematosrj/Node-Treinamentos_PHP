743bf375785870cba51b4a2a887df30bd80ecff2
ENES DA SILVA MATOS <e.matos@pm.ba.gov.br>
2023-12-14 15:17:33 -0300

    ok

---
+++ 07-constantes.php
@@ -0,0 +1,10 @@
+<?php
+//CONSTANTES
+define("NOME", "ENES");
+define("IDADE", 37);
+echo NOME;
+
+echo 'Meu nome é '.NOME.' e minha idade é '.IDADE.'';
+
+
+?>
---
+++ 09-funcoes-array.php
@@ -0,0 +1,17 @@
+<?php
+///IS_ARRAY
+$nomes = array("rodrigo", "Felipe", "maria","jose");
+
+var_dump(is_array($nomes));
+
+if(is_array($nomes)):
+    echo "é um array";
+else:
+    echo "não é um array";
+endif; ////VERIFICA SE UMA DETERMINADA VARIAVEL É UM IS/ARRAY
+///////////////////////////////////////////////////////////////
+$nomes = array("rodrigo", "Felipe", "maria","jose");
+echo in_array()
+
+
+?>
